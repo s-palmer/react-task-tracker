50bcbf33a27c7d6a8d5275c81c632e08c4ce5783
Sergei Palmer <zigpalmer@gmail.com>
2021-11-28 15:01:43 +0000

    Add Task Form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 import { useState } from "react";
 
 
@@ -42,6 +43,7 @@ const App = () => {
   return (
     <div className="container">
       <Header />
+      <AddTask />
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'You have no tasks'}
     </div>
   );
