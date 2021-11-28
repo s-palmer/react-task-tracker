c00a95c10efe8a5c34a854b372e07b2f74098216
Sergei Palmer <zigpalmer@gmail.com>
2021-11-28 15:17:12 +0000

    Hide / Show add task button

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import { useState } from "react";
 
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
+
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -28,6 +30,14 @@ const App = () => {
     },
   ])
 
+  // Add Task
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
   // Delete Task
 
   const deleteTask = (id) => {
@@ -42,8 +52,8 @@ const App = () => {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'You have no tasks'}
     </div>
   );
